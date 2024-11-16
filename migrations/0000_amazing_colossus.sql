b051607b00c15e5d5472f584abfd1afd108736c0
Lola Quiroga <lolaquiroga25@gmail.com>
2024-11-16 11:34:20 -0800

    adding listings table and migrations

---
+++ migrations/0000_amazing_colossus.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS "listings" (
+	"listing_id" serial PRIMARY KEY NOT NULL,
+	"uid" text NOT NULL,
+	"prices" text NOT NULL,
+	"address" text NOT NULL,
+	"city" text NOT NULL,
+	"zip_code" text NOT NULL,
+	"state" text NOT NULL,
+	"picture_ref" text,
+	"feature" jsonb NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"uid" serial PRIMARY KEY NOT NULL,
+	"first_name" text NOT NULL,
+	"last_name" text NOT NULL,
+	"email" text NOT NULL,
+	"is_landlord" boolean NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,17 +1,27 @@
-import { integer, boolean, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, boolean, pgTable, serial, text, timestamp, jsonb } from 'drizzle-orm/pg-core';
 import { em } from 'framer-motion/client';
 
-export const usersTable = pgTable('users_table', {
-  uid: serial('id').primaryKey(),
-  first_name: text('name').notNull(),
-  last_name: text('name').notNull(),
+export const users = pgTable('users', {
+  uid: serial('uid').primaryKey(),
+  first_name: text('first_name').notNull(),
+  last_name: text('last_name').notNull(),
   email: text('email').notNull().unique(),
-  is_landlord: boolean('is_landlord').notNull(),
-
-  
+  is_landlord: boolean('is_landlord').notNull(),  
 });
 
+export const listings = pgTable('listings', {
+    listing_id: serial('listing_id').primaryKey(),
+    uid: text('uid').notNull(),
+    price: text('prices').notNull(),
+    address: text('address').notNull(),
+    city: text('city').notNull(),
+    zip_code: text('zip_code').notNull(),
+    state: text('state').notNull(),
+    picture_ref: text('picture_ref'),
+    feature: jsonb('feature').notNull()
+  });
 
+  
 
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
+export type InsertUser = typeof users.$inferInsert;
+export type SelectUser = typeof users.$inferSelect;
