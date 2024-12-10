6d63a4e7cf5fab81e29ad7040432407e3be34580
Derrick Lee <128185320+DerrickL25@users.noreply.github.com>
2024-12-09 21:59:22 -0800

    improving readability

--- src/app/api/listings/route.ts
+++ src/app/api/listings/route.ts
@@ -10,20 +10,22 @@ export async function POST(request: Request) {
     
     const { feature, media, ...listing } = body;  
 
-   
+    // get coordinates of listing with radar api
     const { latitude, longitude } = await getCoordinates(listing.address);
 
     if (!latitude || !longitude) {
       return NextResponse.json({ error: 'Failed to get coordinates' }, { status: 500 });
     }
 
+    // new listing obj with geo location
     const updatedListing = {
       ...listing,
       latitude: latitude,
       longitude: longitude,
 
     };
-    // Insert the listing into the database
+    
+    // insert the new listing into the database
     await createListing(updatedListing, feature, media);
 
     return NextResponse.json({ message: 'Listing created successfully' }, { status: 201 });
